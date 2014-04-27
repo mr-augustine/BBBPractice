5afee863ad46d45d9d9093b8b327ae3aff486e16
mr-augustine <pushbuttonrobotics@gmail.com>
2014-04-27 08:30:34 -0500

    This is my first commit.

---
+++ HelloBBBUniverse/src/HelloBBBUniverse.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : HelloBBBUniverse.c
+ Author      : mr-augustine
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello Universe!"); /* prints Hello Universe! */
+	return EXIT_SUCCESS;
+}
